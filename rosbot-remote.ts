83df31a1fe62d44f16829d5fec51e882261461a1
Gerard Braad <me@gbraad.nl>
2019-11-29 18:45:37 +0800

    Create rosbot-remote.ts

---
+++ rosbot-remote.ts
@@ -0,0 +1,36 @@
+radio.setGroup(2)
+basic.showLeds(`
+    # . . . #
+    # . . . #
+    . . . . .
+    . . . . .
+    . . . . .
+    `)
+
+basic.forever(function () {
+    let x = pins.analogReadPin(AnalogPin.P1);
+    x = -((x / 2) - 256);
+
+    let direction = ""
+
+    if (x > 128) {
+        direction = "forward"
+    } else if (x < -128) {
+        direction = "backward"
+    } else {
+
+        let y = pins.analogReadPin(AnalogPin.P2);
+        y = -((y / 2) - 256);
+
+        if (y < -128) {
+            direction = "left"
+        } else if (y > 128) {
+            direction = "right"
+        } else {
+            direction = "stop"
+        }
+    }
+
+    //serial.writeLine(direction)
+    radio.sendString(direction)
+})
